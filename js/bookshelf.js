c486bb5f0480c2ab59ed47735963e62afa0b1881
jmowens90 <jmowens90@gmail.com>
2023-05-25 12:16:45 -0400

    added some more comments after finishing addNewBook function

--- js/bookshelf.js
+++ js/bookshelf.js
@@ -23,7 +23,9 @@ class Bookshelf {
         let language = document.getElementById(`language`).value;
         let subject = document.getElementById(`subject`).value;
         let title = document.getElementById(`title`).value;
-    
+
+        // adam said that there may be a way to redo this without needing the if statement
+        // can check into that after the rest of the project is finished/functional
         if (author || language || subject || title) {
             let book = new Book(author, language, subject, title);
             //this.books.push(book); //--> literally kept getting an error that this.books was undefined, took me
@@ -34,8 +36,10 @@ class Bookshelf {
             let ul = document.getElementById(`ul`);
             let li= book.render();
             ul.appendChild(li);
-            // check local storage on mdn to see about keeping the new books added to the database\
+            // check local storage on mdn to see about keeping the new books added to the browser storage
+            // they will stay until the browser is closed, should persist through being refreshed
 
+            //added these next few lines to clear the input fields after the button is pressed
             document.getElementById(`author`).value = ``;
             document.getElementById(`language`).value = ``;
             document.getElementById(`subject`).value = ``;
